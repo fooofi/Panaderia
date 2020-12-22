af28ae8036000876780f705b5c51c7c3e7b5e714
Sebastian Rojas Porras <sarojas.porras@gmail.com>
2020-12-21 23:24:28 -0300

    update install.sh

--- Panaderia/install.sh
+++ Panaderia/install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-echo "================== Install MundoTes Portal =================="
+echo "================== Install Portal =================="
 echo ""
 echo "================== Installing App dependencies =================="
 rm -rf vendor && composer install --no-interaction
 
 echo "================== Installing App Database =================="
 php artisan migrate:fresh --seed
-
-echo "================== Downloading App Environment =================="
-curl -Ls https://cli.doppler.com/install.sh | sh
-DOPPLER_TOKEN=dp.st.dev.LcnCP2pyS9RYpaFlrV4Znt1nA33s7j13rNKzrxPl doppler secrets download --no-file --format=env > .env
 
 echo "============================= COOL!, check the README.md  ============================="
